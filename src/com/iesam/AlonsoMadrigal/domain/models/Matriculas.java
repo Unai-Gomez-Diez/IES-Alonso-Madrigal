abbe8d7cf442f4b58bff892a75fed5bb53bcfb6b
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-02-08 17:25:26 +0100

    creo todas las entidades que faltaban

---
+++ src/com/iesam/AlonsoMadrigal/domain/models/Matriculas.java
@@ -0,0 +1,22 @@
+package com.iesam.AlonsoMadrigal.domain.models;
+
+public class Matriculas {
+    private String id;
+    private String curso;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}

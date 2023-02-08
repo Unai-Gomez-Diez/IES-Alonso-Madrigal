57c58638c47c168928144dfb2256b49d8a287ae2
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-02-08 10:15:21 +0100

    Creando el entidades

---
+++ src/com/iesam/AlonsoMadrigal/domain/models/Clase.java
@@ -0,0 +1,31 @@
+package com.iesam.AlonsoMadrigal.domain.models;
+
+public class Clase {
+    private String id;
+    private String planta;
+    private Integer capacidad;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(String planta) {
+        this.planta = planta;
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+}
---
+++ src/com/iesam/AlonsoMadrigal/domain/models/Clase_ordenadores.java
@@ -0,0 +1,22 @@
+package com.iesam.AlonsoMadrigal.domain.models;
+
+public class Clase_ordenadores extends Clase{
+    private Integer nºordenadores;
+    private String tipo_ordenadores;
+
+    public Integer getNºordenadores() {
+        return nºordenadores;
+    }
+
+    public void setNºordenadores(Integer nºordenadores) {
+        this.nºordenadores = nºordenadores;
+    }
+
+    public String getTipo_ordenadores() {
+        return tipo_ordenadores;
+    }
+
+    public void setTipo_ordenadores(String tipo_ordenadores) {
+        this.tipo_ordenadores = tipo_ordenadores;
+    }
+}
